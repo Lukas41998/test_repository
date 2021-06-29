8b5c0bb950189ad4ab23d20edc61be349fc3be59
lukas41998 <lukas.eichelberger@student.unisg.ch>
2021-06-29 10:40:47 +0200

    hello comment

--- git_test_file.py
+++ git_test_file.py
@@ -1,5 +1,3 @@
 # Noting to add
 for i in range(100):
-    print("hello world" + str(i))
-    print("This is a jokezzz")
-    print("This is the next line i want to add because I am a motherfucker")
+    pass
